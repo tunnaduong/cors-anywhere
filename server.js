4e1556b401980ce5e5784affc8d29ebe0143c8a2
Dương Tùng Anh <tunnaduong@gmail.com>
2024-10-18 03:51:27 +0700

    Update server.js

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ cors_proxy.createServer({
     // Set a fixed cookie for the response
     // Thêm cookie vào yêu cầu đến server gốc
     req.headers['Cookie'] = 'PHPSESSID=' + process.env.PHPSESSID +  '; Path=/;'; // Thay đổi giá trị cookie theo nhu cầu
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Use '*' or your specific domain
+    res.setHeader('Access-Control-Allow-Origin', 'https://diemthi.tunnaduong.com'); // Use '*' or your specific domain
     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Important for sending cookies
   },
 }).listen(port, host, function() {
